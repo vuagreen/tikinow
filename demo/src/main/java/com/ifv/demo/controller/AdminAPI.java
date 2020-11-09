6c773da864c5db5d4df67b7d11ac1e9f1077e565
vyvuvan <vyvuvan95@gmail.com>
2020-11-09 14:49:22 +0700

    update 1-2h30

--- demo/src/main/java/com/ifv/demo/controller/AdminAPI.java
+++ demo/src/main/java/com/ifv/demo/controller/AdminAPI.java
@@ -1,18 +1,59 @@
 package com.ifv.demo.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ifv.demo.model.User;
+import com.ifv.demo.service.ManagerUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/admin")
 public class AdminAPI {
+
+    @Autowired
+    private ManagerUserService managerUserService;
+
     @GetMapping("/")
     public String hello() {
         return "Hello Admin";
     }
-    @GetMapping("/abc")
-    public String hello2() {
-        return "Hello Admin";
-    }
+
+    @PostMapping("/acount")
+    public Map<String, Object> create (@RequestBody User user){
+        Map<String, Object> result = new HashMap<String, Object>();
+        User u = managerUserService.Create(user);
+        result.put("user", u);
+        return result;
+    };
+
+    @GetMapping("/acount{id}")
+    public Map<String, Object> select (@PathVariable("id")  Long id){
+        Map<String, Object> result = new HashMap<String, Object>();
+        Optional<User> u = managerUserService.FindUserById(id);
+        result.put("user", u);
+        return result;
+    };
+
+    @PutMapping("/acount")
+    public Map<String, Object> edit (@RequestBody User user){
+        Map<String, Object> result = new HashMap<String, Object>();
+        User u = managerUserService.Create(user);
+        result.put("user", u);
+        return result;
+    };
+
+    @DeleteMapping("/acount/{id}")
+    public Map<String, Object> delete (@PathVariable("id") Long id){
+        Map<String, Object> result = new HashMap<String, Object>();
+        try {
+            managerUserService.Delete(id);
+            result.put("mesager", "Xóa người dùng thành công!");
+        } catch (Exception e) {
+            result.put("mesager", "Xóa người dùng thất bại!");
+        }
+        return result;
+    };
 }
